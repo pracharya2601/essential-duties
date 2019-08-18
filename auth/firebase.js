afe58703af236cf0e8af260d97648d572a9879ca
Prakash Acharya <pracharya2601@gmail.com>
2019-08-17 23:11:21 -0700

    project upload

---
+++ auth/firebase.js
@@ -0,0 +1,15 @@
+var fireBase = fireBase || firebase;
+var hasInit = false;
+var config = {
+    apiKey: "AIzaSyC48M6kJwSUHCGrKaeQWOIf5M7TEtBWg4k",
+    authDomain: "my-own-project-661c3.firebaseapp.com",
+    databaseURL: "https://my-own-project-661c3.firebaseio.com",
+    projectId: "my-own-project-661c3",
+    storageBucket: "",
+    messagingSenderId: "62789234988",
+    appId: "1:62789234988:web:dc1a13890b11bb96"
+  };
+if(!hasInit){
+    firebase.initializeApp(config);
+    hasInit = true;
+}
